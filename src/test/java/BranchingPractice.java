2405a109e4a4d6855ba6379fe5d3b95375728db3
Burak <burakaltintas01@gmail.com>
2021-05-25 18:23:44 -0500

    things we've done so far and shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,5 +1,9 @@
 public class BranchingPractice {
     public static void main(String[] args) {
-
+        //created new project
+        // enabled version control
+        // added .ignorefile , target and .iml file
+        // made initial commit by keeping track of all files
+        //shared project on github
     }
 }
